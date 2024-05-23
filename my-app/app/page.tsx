de5e375608297e413a778faa14d1baa8e0d1ee52
RyuzakiiL23 <elhamrani.omar23@gmail.com>
2024-05-23 23:11:52 +0100

    main branch

---
+++ my-app/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main className="text-6xl font-bold w-full h-screen relative">
+      <div className="flex items-center justify-center h-full">
+        <h1>Main Branch</h1>
+      </div>
+    </main>
+  );
+}
