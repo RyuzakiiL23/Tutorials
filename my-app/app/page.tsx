87829e4a778ed339747ff0da89d614cf83e1c1b5
RyuzakiiL23 <elhamrani.omar23@gmail.com>
2024-05-23 23:13:41 +0100

    e-com branch

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <main className="text-6xl font-bold w-full h-screen relative">
       <div className="flex items-center justify-center h-full">
-        <h1>Main Branch</h1>
+        <h1>E-com Branch</h1>
       </div>
     </main>
   );
