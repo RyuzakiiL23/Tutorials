4593a051d0d843aefdfafbebc70d0243c20ebc57
RyuzakiiL23 <elhamrani.omar23@gmail.com>
2024-05-24 02:15:31 +0100

    Setting up NextUI

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -1,8 +1,13 @@
+import { Button } from "@nextui-org/button";
 export default function Home() {
   return (
     <main className="text-6xl font-bold w-full h-screen relative">
       <div className="flex items-center justify-center h-full">
         <h1>E-com Branch</h1>
+
+        <div>
+          <Button color="primary">Button</Button>
+        </div>
       </div>
     </main>
   );
